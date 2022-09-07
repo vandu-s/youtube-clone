2a9d022c9fd5265bf0dd105ec76276c83f0df9c7
vandu-s <vandana.singandipe@gmail.com>
2022-09-07 16:41:00 +0530

    first commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+
+const SearchBar = () => {
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;
