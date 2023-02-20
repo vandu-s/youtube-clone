1a99813f2c39bdab4d07b0c26c2129be93e5d064
vandu-s <vandana.singandipe@gmail.com>
2023-02-20 12:45:25 +0000

    card width changed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardMedia } from '@mui/material';
-import { CheckCircle } from '@mui/icons-material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 import {
   demoThumbnailUrl,
   demoVideoUrl,
   demoVideoTitle,
   demoChannelUrl,
   demoChannelTitle,
-} from '../utils/constants';
+} from "../utils/constants";
 
 const VideoCard = ({
   video: {
@@ -19,19 +19,19 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { xs: '100%', sm: '358px', md: '320px' },
-        boxShadow: 'none',
-        borderRadius: '0',
+        width: { xs: "100%", sm: "358px" },
+        boxShadow: "none",
+        borderRadius: "0",
       }}
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
+          sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
         />
       </Link>
-      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant="subtitle1" fontWeight="bold" color="#fff">
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
@@ -47,7 +47,7 @@ const VideoCard = ({
           <Typography variant="subtitle2" fontWeight="bold" color="gray">
             {snippet?.channelTitle.slice(0, 60) ||
               demoChannelTitle.slice(0, 60)}
-            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
           </Typography>
         </Link>
       </CardContent>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -11,7 +11,7 @@ const Videos = ({ videos, direction }) => {
       flexWrap="wrap"
       justifyContent="start"
       alignItems="start"
-      gap={2}
+      gap="30px"
     >
       {videos.map((item, idx) => (
         <Box key={idx}>
